b28bc09bb96cea7bec9f60c43eafbc54e46c2e14
kegham <kegham1983@gmail.com>
2016-05-18 13:57:50 +0200

    Initilizing app

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('./', function (req, res) {
+	res.send('Todo api root is working!');
+});
+
+app.listen(port, function (){
+	console.log('Server listeining on port ' + port + '...');
+});
